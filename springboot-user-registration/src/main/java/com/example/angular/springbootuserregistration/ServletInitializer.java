cc1871adedb743f231a8a94e3b665d39fa2623c7
Moumita Chakraborty <37339380+Moumita110@users.noreply.github.com>
2021-07-13 12:11:00 +0600

    Add files via upload

---
+++ springboot-user-registration/src/main/java/com/example/angular/springbootuserregistration/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.angular.springbootuserregistration;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootUserRegistrationApplication.class);
+	}
+
+}
